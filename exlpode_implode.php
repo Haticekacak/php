8b5e4db4df4924b1f24a2c942c2093634624c157
Hatice Kaçak <101213468+Haticekacak@users.noreply.github.com>
2024-05-16 21:38:43 +0300

    Update exlpodephp

--- exlpode_implode.php
+++ exlpode_implode.php
@@ -5,11 +5,13 @@
     <title>GET VE POST </title>
 </head>
 <body>
-<p>
-   her iki method da kullanıcıdan alınan verileri ileetmek veri göndermek için kullanılır.
-</p><br>
-<p>post methodu veri güvenliği açısından daha ön plandadır.Çünkü GET methodu verileri adres çubuğundan gönderir buda güvenlik açığına neden olur</p>
-<p>$_GET  VE $_POST    </p>
+<p>diziyi metine veya metini dizye çevirmemizi sağlayan bir yapıdır</p>
+<?php
+$metin = "şimdi kaybolan yıllarımı verseler çeyrek asrı tam önüme serseler"
+$d=explode(" ",$metin);
+print_r($d);
+?>
+<p>boşluk içeren kısımlerı keserek metnimizi diziye çevirecek</p>
 
 </body>
 </html>
